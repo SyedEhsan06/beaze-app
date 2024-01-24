b39498f96356204c54c8a6fc791cb55318e4b762
Ehsan <ctech1699@gmail.com>
2024-01-24 22:06:46 +0530

    category fixed

--- src/app/api/category/route.js
+++ src/app/api/category/route.js
@@ -1,19 +1,30 @@
 import Category from "@/lib/models/category";
 import { connectToDb } from "@/lib/utils";
-connectToDb();
-export async function GET() {
-  const categories = await Category.find({});
-  return Response.json({ categories });
-}
-export async function POST(req){
-  try{
-    const data = await req.json();  
-    const category = Category.insertMany(data);
-    return Response.json({ category });
-  }
-  catch(err){
-    return Response.json({ err });
-  }
-  return Response.json({ msg: "Category created" });
 
+connectToDb();
+
+export async function GET() {
+  try {
+    const categories = await Category.find({});
+    return Response.json({ categories });
+  } catch (err) {
+    return Response.json({ error: err.message });
+  }
+}
+
+export async function POST(req) {
+  try {
+    const data = await req.json();
+  
+    const existingCategory = await Category.findOne({ name: data.name });
+
+    if (existingCategory) {
+      return Response.json({ error: "Category already exists" });
+    }
+
+    const category = await Category.create(data);
+    return Response.json({ category, msg: "Category created" });
+  } catch (err) {
+    return Response.json({ error: err.message });
+  }
 }
