8bc33cecbeea8a8f9c7c0b09ca53ed39952f9f17
Ehsan <ctech1699@gmail.com>
2024-02-26 00:36:32 +0530

    sidebar fix

--- src/app/api/auth/cart/route.js
+++ src/app/api/auth/cart/route.js
@@ -51,22 +51,7 @@ export async function DELETE(req) {
 }
 //update product quantity in cart
 // Clear user's cart
-export async function DELETE(req) {
-    const { userId } = await req.json();
-    try {
-        let user = await User.findOne({ _id: userId });
-        if (!user) {
-            return Response.json({ message: "User not found" });
-        } else {
-            user.cart = [];
-            await user.save();
-            return Response.json({ message: "Cart cleared successfully" });
-        }
-    } catch (error) {
-        console.error("Cart error:", error);
-        return Response.json({ error: "Internal Server Error" });
-    }
-}
+// 
 // get user's cart
 export async function GET(req) {
     const { userId } = await req.json();
